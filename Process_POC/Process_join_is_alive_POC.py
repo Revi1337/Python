a05add0fa394baa4b188be1878a3e2d0420fbabf
Revi1337 <david122123@gmail.com>
2022-08-15 14:17:11 +0900

    append ThreadPOC

---
+++ Process_POC/Process_join_is_alive_POC.py
@@ -0,0 +1,41 @@
+import logging
+from multiprocessing import Process, current_process
+import time
+import random 
+import os
+
+"""
+Author : revi1337
+Date : 2022-08-15
+"""
+
+def task(sec):
+    child_process_name = current_process().name
+    child_process_pid = current_process().pid
+    print("Named {} Child Process Started (PID : {} , sleep {} seconds)".format(child_process_name, child_process_pid, sec))
+    time.sleep(sec)
+    print("Named {} Child Process Finished!".format(child_process_name))
+
+
+def main():
+    logging.info("[+] Parent Process Started ( PID : {})".format(os.getpid()))    
+    print()
+
+    logging.info("[+] Several Child Processes  Created")
+    processes = list()
+    for i in range(4):
+        sec = random.randint(1,7)
+        p = Process(name = str(i) , target = task, args = (sec,))
+        p.start()
+        processes.append(p)
+    for process in processes:
+        process.join()
+
+    print()
+    logging.info("[+] Parent Process Finished. .join() methods wait for Child-Process til done")
+    print()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format = "%(asctime)s %(message)s", level = logging.INFO, datefmt = "%H:%M:%S")
+    main()
